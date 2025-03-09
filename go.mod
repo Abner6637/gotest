89ebb596440911a2333d942d3afbb10df95f3a22
Abner6637 <abner66374323@gmail.com>
2025-03-09 06:54:15 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gotest
+
+go 1.23.0
+
+toolchain go1.23.7
+
+require (
+
+)
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"gotest/calc"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	result := calc.Add(3, 5)
+	fmt.Println("Result:", result)
+}
