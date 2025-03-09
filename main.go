3dcd56cafa707eddb676501f963e25cbca4c877f
Abner6637 <abner66374323@gmail.com>
2025-03-09 07:26:05 +0000

    Add new feature

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
-	result := calc.Add(2, 4)
+	result := calc.Add(3, 5)
+	result2 := calc.Add(2, 4)
 	fmt.Println("Result:", result)
+	fmt.Println("Result:", result2)
 }
